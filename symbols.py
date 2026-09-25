ea95c1b9f4c32f7bce0f52aeec5e1d5a10ddda40
Miguel Avila de Oliveira <miguelaviladeoliveira@gmail.com>
2026-09-25 12:51:03 -0300

    adiciona codigos ansi de cores para o terminal

--- symbols.py
+++ symbols.py
@@ -1,5 +1,12 @@
 from enum import StrEnum
 
+#cores ansi para o terminal
+RESET  = "\033[0m"
+RED    = "\033[31m"
+GREEN  = "\033[32m"
+YELLOW = "\033[33m"
+CYAN   = "\033[36m"
+
 #enum representando a direção de um shift de uma cabeça da máquina
 class Shift(StrEnum):
     RIGHT = "R"
